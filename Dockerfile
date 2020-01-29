6d4e64725631e96efd5b0e4aad83d10792b49010
operatorequals <john.torakis@gmail.com>
2020-01-29 23:49:42 +0200

    Dockerfile working

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,17 @@
 # Use different image for runtime
 FROM python:3.7-slim AS cluster_dict_serve:v1
 
+WORKDIR /home/appuser
+
+COPY cluster_dict /home/appuser/cluster_dict
+COPY setup.py .
+COPY requirements.txt .
+
+RUN pip install -r requirements.txt && python setup.py install
+
 # Create non-root user (Security First!)
 RUN useradd --create-home appuser
 WORKDIR /home/appuser
 USER appuser
 
-COPY cluster_dict /home/appuser
-
-CMD [ "python3 -m cluster_dict serve" ]
+# CMD [ "python3 -m cluster_dict serve" ]
--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 from setuptools import setup
 
-
 import cluster_dict
 
 try :
@@ -8,47 +7,54 @@ try :
 except IOError as e:
 	long_description_str = 'https://github.com/operatorequals/cluster_dict/blob/master/README.md'
 
+def load_requirements(fname):
+    reqs = parse_requirements(fname, session="test")
+    return [str(ir.req) for ir in reqs]
+
+setup(
+	name=cluster_dict.__name__,
+	version=cluster_dict.__version__,
+	description='Distributed Python dict',
+	long_description=long_description_str,
+	author=cluster_dict.__author__,
+	author_email='john.torakis@gmail.com',
+	license='Apache2',
+	url=cluster_dict.__github__,
+	py_modules=['cluster_dict'],
+
+	# install_requires=load_requirements("requirements.txt"),
+
+	classifiers=[
+	# 'Development Status :: 6 - Mature',
+
+	'License :: OSI Approved :: Apache Software License',
+
+	'Programming Language :: Python :: 2.7',
+	'Programming Language :: Python :: 3.4',
+	'Programming Language :: Python :: 3.7',
+
+	'Programming Language :: Python :: Implementation :: CPython',
+	'Programming Language :: Python :: Implementation :: PyPy',
+
+		'Intended Audience :: Developers',
+	'Intended Audience :: Information Technology',
+
+	'Topic :: Software Development :: Libraries :: Python Modules',
+	'Topic :: Software Development :: Build Tools',
+	'Topic :: Software Development :: Testing',
 
 
-setup(name='cluster_dict',
-	  version=cluster_dict.__version__,
-	  description='Distributed Python dict',
-	  long_description=long_description_str,
-	  author=cluster_dict.__author__,
-	  author_email='john.torakis@gmail.com',
-	  license='Apache2',
-	  url=cluster_dict.__github__,
-	  py_modules=['cluster_dict'],
-	  classifiers=[
-		# 'Development Status :: 6 - Mature',
-
-		'License :: OSI Approved :: Apache Software License',
-
-		'Programming Language :: Python :: 2.7',
-		'Programming Language :: Python :: 3.4',
-		'Programming Language :: Python :: 3.7',
-
-		'Programming Language :: Python :: Implementation :: CPython',
-		'Programming Language :: Python :: Implementation :: PyPy',
-
- 		'Intended Audience :: Developers',
-		'Intended Audience :: Information Technology',
-
-		'Topic :: Software Development :: Libraries :: Python Modules',
-		'Topic :: Software Development :: Build Tools',
-		'Topic :: Software Development :: Testing',
-
-		
 
 	],
-	keywords = ['dict',
+	keywords = [
+		'dict',
 		'distributes',
 		'key-value',
-		],
+	],
 
 	# entry_points = {
 	# 	'console_scripts' : [
-	# 			"cluster_dict="
-	# 		]
-	# 	}
-	)
+	# 			"cluster_dict=cluster_dict.__main__:"
+	# 	]
+	# }
+)
